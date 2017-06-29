4737853d959ea13a25b8b4c78f558438bee98314
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-06-29 11:28:40 -0600

    modified get and post route, added data

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+var instagrams = [
+  { title: "My Dog", date: makeDate(10), img: "https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Shake-shiver-and-tremble-Why-dogs-do-it.jpg?itok=yvOUgQeL" },
+  { title: "Also my dog", date: makeDate(8), img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04cM7atojGKtFOZTukDBBqvVy13OgO3ZPrib39rtHEE_qGhdh" },
+  { title: "Not my dog", date: makeDate(7), img: "http://barkingroyalty.com/wp-content/uploads/2015/12/Beagle-puppy.jpg?x30644" },
+]
+
 app.get('/instagrams', function(req,res){
-  res.send("Hi");
+  res.json({ message: "Found Data", data:instagrams })
 });
 
 app.post('/instagrams', function(req,res){
-  res.send("Hello from post!");
+
+  var newGram = { title: req.body.title, img: req.body.img, date: makeDate(0) };
+
+  instagrams.push(newGram);
+
+  res.json({ message: "successfully added data", data: instagrams });
 });
 
 
