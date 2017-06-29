e778d235e6220966d4316586d7dc7366ba72bf04
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-06-29 13:13:26 -0600

    added methods to fetch data and post data using vue

--- public/app.js
+++ public/app.js
@@ -2,8 +2,43 @@ var apiUrl = "/instagrams";
 
 var component = new Vue({
   el: "#app",
-  data: {},
-  methods: {}
+  data: {
+    instagrams: null,
+    newTitle: null,
+    newImage: null
+  },
+  created: function(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function(){
+      var self = this;
+
+      $.ajax({
+        url: apiUrl,
+        method: 'GET'
+      }).done(function(response){
+        self.instagrams = response.data;
+        console.log(self.instagrams);
+      })
+    },
+    submitInstagram: function(){
+      var self = this;
+      var item = {
+        title: this.newTitle,
+        img: this.newImage
+      }
+      $.ajax({
+        url: apiUrl,
+        method: 'POST',
+        data: item
+      }).done(function(response){
+        self.fetchData();
+        console.log(response);
+      })
+
+    }
+  }
 });
 
 console.log(component);
