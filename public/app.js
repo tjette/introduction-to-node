92fa12407f1ff93a3f960a7c1800571f5d52b985
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-01 07:02:22 -0600

    made changes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,29 @@
 var apiUrl = "/instagrams";
 
+
+// var instagramcomponent = new Vue ({
+//   el: "#insta",
+//   data: {
+//     photos: []
+//   },
+//   created: function(){
+//     this.fetchGrams();
+//   },
+//   methods: {
+//   fetchGrams: function(){
+//     var self = this;
+//   $.ajax({
+//     url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=13591390.01bf291.8573ab6f3adf42f8ac53375c94005742',
+//     method: 'GET'
+//   }).done(function(response){
+//     self.photos=response;
+//     console.log(self.photos);
+//   })
+// }
+// }
+// });
+// console.log(instagramcomponent);
+
 var component = new Vue({
   el: "#app",
   data: {
@@ -40,5 +64,5 @@ var component = new Vue({
     }
   }
 });
-
+//13591390.01bf291.8573ab6f3adf42f8ac53375c94005742 access token
 console.log(component);
